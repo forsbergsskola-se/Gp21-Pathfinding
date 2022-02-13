7458150eb93703dcdba4f8b0295f61db1719c6c6
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-02-13 22:42:52 +0100

    Rigidbody, playermovement, dashing

--- Gp21 PathFinding/Assets/Scripts/PlayerController.cs
+++ Gp21 PathFinding/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody _rb;
+    public float moveSpeed = 5f;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        dashTime = startDashTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //movement input vertical and horizontally
+        var horInput = Input.GetAxis("Horizontal");
+        var vertInput = Input.GetAxis("Vertical");
+        //Set move velocity
+        _rb.velocity = new Vector3(horInput * moveSpeed, _rb.velocity.y, vertInput * moveSpeed);
         
+        //Checking if dashing, if not, then checking if currently pressing dash input(V)
+        if (direction == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Assigning direction no. to each input direction
+                if (horInput < 0)
+                {
+                    direction = 1;
+                }
+                else if (horInput > 0)
+                {
+                    direction = 2;
+                }
+                else if (vertInput < 0)
+                {
+                    direction = 3;
+                }
+                else if (vertInput > 0)
+                {
+                    direction = 4;
+                }
+            }
+        }
+        else
+        {
+            //"Resetting" dash values at the end of dash/before start of dash
+            if (dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                _rb.velocity = Vector3.zero;
+            }
+            else
+            {
+                //Setting dashtime to decrease slowly over time
+                dashTime -= Time.deltaTime;
+                //Adding velocity to each dash direction
+                if (direction == 1)
+                {
+                    _rb.velocity = Vector3.left * dashSpeed;
+                }
+                else if (direction == 2)
+                {
+                    _rb.velocity = Vector3.right * dashSpeed;
+                }
+                else if (direction == 3)
+                {
+                    _rb.velocity = Vector3.back * dashSpeed;
+                }
+                else if (direction == 4)
+                {
+                    _rb.velocity = Vector3.forward * dashSpeed;
+                }
+            }
+        }
     }
 }
